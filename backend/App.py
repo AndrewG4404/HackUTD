4650f184a719030815462b59fea57529d413c98e
Andrew Gonzalez <gonzalezandrew528@gmail.com>
2024-11-16 18:13:33 -0600

    hello
    hello

--- backend/App.py
+++ backend/App.py
@@ -9,3 +9,4 @@ def get_crypto_predictions(coin: str, days: int = 90, future_days: int = 7):
     model, scaler, data = train_model(coin, days=days, epochs=5)
     predictions = predict_prices(model, scaler, data, future_days=future_days)
     return predictions.to_dict(orient="records")
+#h4llo
